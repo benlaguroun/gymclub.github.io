cd38b6f4e7adf09580c127f13fa57bd26037f6e1
benlaguroun <benlaguroun@gmail.com>
2024-02-15 20:58:34 +0100

    js file

---
+++ script.js
@@ -0,0 +1,11 @@
+// scrolling navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
